9ee3a8a3b4014161d1733132ce34a19f8cd98880
Kumar Devanshu <devanshukd007@gmail.com>
2023-11-28 21:00:59 +0530

    fragments-demo

---
+++ 2. React and Redux/4 fragments/fragments-demo/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Healthy Foods</h1>
+      <ul class="list-group">
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Green Vegetables</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Salad</li>
+        <li class="list-group-item">Milk</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
